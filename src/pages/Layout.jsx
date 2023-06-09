0f2b7887ed0ec2916c02da4137490f3e9f4c4676
Kamil Sobik <kamil.sobik@gmail.com>
2023-06-09 10:33:22 +0200

    config firebase and api credentials

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -5,14 +5,12 @@ import Header from "../components/Header";
 
 const Layout = () => {
   return (
-    <div>
-      <div className="w-screen h-screen flex">
-        <Sidebar />
-        <div className="h-screen flex-1">
-          <Header />
-          <div className="justify-center items-center">
-            <Outlet />
-          </div>
+    <div className="w-screen h-screen flex">
+      <Sidebar />
+      <div className="h-screen flex-1">
+        <Header />
+        <div className="justify-center items-center">
+          <Outlet />
         </div>
       </div>
     </div>
