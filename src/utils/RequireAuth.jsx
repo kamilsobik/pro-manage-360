00220aa05b6c1fcbba60c207314d9045d12b1083
Kamil Sobik <kamil.sobik@gmail.com>
2023-06-09 21:54:02 +0200

    protecting route - visible only for logged user

---
+++ src/utils/RequireAuth.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, getAuth } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { FaSpinner } from "react-icons/fa";
+
+const RequireAuth = ({ children }) => {
+  const [authenticated, setAuthenticated] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(
+      auth,
+      (user) => {
+        if (user) {
+          setAuthenticated(user);
+          setLoading(false);
+          return;
+        }
+        setAuthenticated(null);
+        setLoading(false);
+      },
+      (error) => {
+        throw new Error(error);
+      }
+    );
+  }, []);
+
+  if (!authenticated && loading) {
+    return (
+      <p>
+        <FaSpinner />
+      </p>
+    );
+  }
+
+  if (!authenticated && !loading) {
+    return navigate("/signin");
+  }
+
+  return children;
+};
+
+export default RequireAuth;
